ad2d33ca1ebcb25612ae7160a7f0446c88a02819
Thomas Sve <thomas.sve@transituc.com>
2018-05-06 12:36:27 -0300

    Started adding model and train logic

---
+++ model.py
@@ -0,0 +1,40 @@
+"""
+
+"""
+
+from keras.models import Sequential
+from keras.layers import Convolution2D, Flatten, Dense, Lambda, Dropout, MaxPooling2D
+
+
+
+def lenet():
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=input_shape)) # normalize data
+    model.add(Convolution2D(6, 5, 5, activation = 'relu'))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6, 5, 5, activation = 'relu'))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dropout(keep_prob))
+    model.add(Dense(84))
+    model.add(Dense(1))
+    return model
+
+
+def nvidia_cnn(input_shape, keep_prob = 0.5):
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=input_shape)) # normalize data
+    model.add(Convolution2D(24, 5, 5, activation = 'relu', subsample=(2, 2)))
+    model.add(Convolution2D(36, 5, 5, activation = 'relu', subsample=(2, 2)))
+    model.add(Convolution2D(48, 5, 5, activation = 'relu', subsample=(2, 2)))
+    model.add(Convolution2D(64, 3, 3, activation = 'relu'))
+    model.add(Convolution2D(64, 3, 3, activation = 'relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dropout(keep_prob))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    return model
+    
---
+++ train_behavioral.py
@@ -0,0 +1,39 @@
+
+import os
+import os.path as osp
+import sys
+import numpy as np
+
+def generator(data):
+    pass
+
+def train_model(batch = 64, epochs = 100, learning_reate = 0.001):
+    pass
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train the behavioral model')
+    parser.add_argument('--batch', dest='batch',
+                        help='batch size to train',
+                        default=64, type=int)
+    parser.add_argument('--epochs', dest='epochs',
+                        help='number of epochs to run over training set',
+                        default=100, type=int)
+    parser.add_argument('--l', dest='learning_rate',
+                        help='set the learning rate of model',
+                        default=0.001, type=int)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    """
+    Train behavioral model
+    """
+
+    args = parse_args()
+    train_model()
+    
